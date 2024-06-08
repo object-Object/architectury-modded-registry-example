3842500ac7b8b7d795beb2f9b7ab62c5c6af908a
object-Object <object@objectobject.ca>
2024-06-08 15:24:53 -0400

    Clean up imports

--- common/src/main/java/net/examplemod/ExampleMod.java
+++ common/src/main/java/net/examplemod/ExampleMod.java
@@ -1,18 +1,5 @@
 package net.examplemod;
 
-import com.google.common.base.Suppliers;
-import dev.architectury.registry.CreativeTabRegistry;
-import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.RegistrarManager;
-import dev.architectury.registry.registries.RegistrySupplier;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-
-import java.util.function.Supplier;
-
 public class ExampleMod {
     public static final String MOD_ID = "examplemod";
 
